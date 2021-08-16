2c3b5e9842cd45ecc86374988da01d43c03193cf
LawrenceMoruye <moruyelawrence@gmail.com>
2021-08-16 21:07:16 +0000

    environment set up

---
+++ resnet/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='implementation of resnet paper using pytorch',
+    author='Lawrence_Moruye',
+    license='MIT',
+)
